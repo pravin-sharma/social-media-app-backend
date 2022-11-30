0193db323bacb0b590c388650235f3c8433b64bb
PRAVIN SHARMA <pravinsharma9610@gmail.com>
2022-11-30 19:18:57 +0530

    added dob and gender

--- models/profile.js
+++ models/profile.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 
-const profileSchema = new mongoose.Schema(
-  {
-    user: { type: Schema.Types.ObjectId, ref: "User" },
-    bio: { type: String, required: true },
-    social: {
-      facebook: { type: String },
-      twitter: { type: String },
-      youtube: { type: String },
-      instagram: { type: String },
+const profileSchema = new mongoose.Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    bio: { type: String },
+    dob: {
+      type: Date
     },
+    gender: {
+      type: String,
+      enum: ['male', 'female', 'other']
+    }
   },
   { timestamps: true }
 );
