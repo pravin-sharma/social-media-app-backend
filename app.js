59d2f06375d2070e12073f588bff668fc3a00e40
PRAVIN SHARMA <pravinsharma9610@gmail.com>
2022-11-30 18:50:43 +0530

    added user route and Custom error handler

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const cors = require('cors');
+const cors = require("cors");
+const userRoute = require("./routes/user");
+const customErrorHandler = require("./utils/customErrorHandler");
 
-app.use(express.json())
+app.use(express.json());
 // app.use(express.urlencoded())
-app.use(cors())
+app.use(cors());
 
+//routes
+app.use("/", userRoute);
+
+//error handler
+app.use(customErrorHandler)
 
 module.exports = app;
