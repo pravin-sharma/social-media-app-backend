4001329b026a70d038d4b9c3140706b219f970e3
PRAVIN SHARMA <pravinsharma9610@gmail.com>
2022-12-01 23:24:21 +0530

    route added for posts and invalid path

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 
 const cors = require("cors");
 const userRoute = require("./routes/user");
+const postRoute = require("./routes/post");
 const customErrorHandler = require("./utils/customErrorHandler");
+const CustomError = require("./utils/CustomError");
 
 app.use(express.json());
 // app.use(express.urlencoded())
@@ -11,6 +13,9 @@ app.use(cors());
 
 //routes
 app.use("/", userRoute);
+app.use("/", postRoute);
+
+app.use("*", (req,res,next)=>next(CustomError.notFound('Invalid API Path')))
 
 //error handler
 app.use(customErrorHandler)
