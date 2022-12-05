b7956636384b2db5ccc08176f34f549f60a9c432
PRAVIN SHARMA <pravinsharma9610@gmail.com>
2022-12-05 15:24:13 +0530

    trending post route

--- routes/post.js
+++ routes/post.js
@@ -1,11 +1,21 @@
-const { addPost, getAllPosts, getOnePost, deletePost, likePost, addComment, removeComment, updateComment } = require('../controllers/post');
+const { addPost, getAllPosts, getOnePost, deletePost, likePost, addComment, removeComment, updateComment, trendingPost } = require('../controllers/post');
 const { isAuth } = require('../middleware/isAuth');
 
 const router = require('express').Router();
 
+// add a post
 router.post('/post', isAuth, addPost);
+
+// get all posts
 router.get('/post/all', isAuth, getAllPosts);
+
+//trending posts
+router.get('/post/trending', isAuth, trendingPost);
+
+// get post by id
 router.get('/post/:postId', isAuth, getOnePost);
+
+// delete post by id
 router.delete('/post/:postId', isAuth, deletePost);
 
 //add like
@@ -21,7 +31,7 @@ router.delete('/post/comment/:postId/:commentId', isAuth, removeComment)
 router.put('/post/comment/:postId/:commentId', isAuth, updateComment)
 
 //disable post
-//view all posts
+
 
 module.exports = router;
 
