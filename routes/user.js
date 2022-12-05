452cc4c6ac094862b8f6153a180653202611285b
PRAVIN SHARMA <pravinsharma9610@gmail.com>
2022-12-05 15:22:40 +0530

    get user by id

--- routes/user.js
+++ routes/user.js
@@ -1,42 +1,55 @@
-const { Router } = require('express');
-const router = Router()
+const { Router } = require("express");
+const router = Router();
 
-const {signUp, login, getUser, updateUser, emailVerification, initPasswordReset, perfPasswordReset, getAllUser, deleteUser, disableUser, enableUser} = require('../controllers/user');
-const { isAdmin } = require('../middleware/isAdmin');
-const { isAuth } = require('../middleware/isAuth');
+const {
+  signUp,
+  login,
+  getUser,
+  updateUser,
+  emailVerification,
+  initPasswordReset,
+  perfPasswordReset,
+  getAllUser,
+  deleteUser,
+  disableUser,
+  enableUser,
+  getUserById
+} = require("../controllers/user");
+const { isAdmin } = require("../middleware/isAdmin");
+const { isAuth } = require("../middleware/isAuth");
 
-
-router.post('/signup', signUp);
-router.post('/login', login);
+router.post("/signup", signUp);
+router.post("/login", login);
 
 //Get Logged In User
-router.get('/user', isAuth, getUser);
+router.get("/user", isAuth, getUser);
 
 //Update Logged In User
-router.put('/user', isAuth, updateUser);
+router.put("/user", isAuth, updateUser);
 
 // email verification
-router.get('/user/verify/:verificationHash', emailVerification);
+router.get("/user/verify/:verificationHash", emailVerification);
 
 // initiate password reset
-router.post('/user/passwordReset', initPasswordReset);
+router.post("/user/passwordReset", initPasswordReset);
 
 //perform Password Reset
-router.post('/user/passwordResetPerform', perfPasswordReset);
+router.post("/user/passwordResetPerform", perfPasswordReset);
 
 // view all users except admin
-router.get('/user/all', isAuth, getAllUser )
+router.get("/user/all", isAuth, getAllUser);
 
-//admin 
+// get a user by id
+router.get("/user/:userId", isAuth, getUserById);
+
+//admin
 //delete user
-router.delete('/user/:userId', isAuth, isAdmin, deleteUser )
+router.delete("/user/:userId", isAuth, isAdmin, deleteUser);
 
 //disable user
-router.get('/user/disable/:userId', isAuth, isAdmin, disableUser )
+router.get("/user/disable/:userId", isAuth, isAdmin, disableUser);
 
 //enable user
-router.get('/user/enable/:userId', isAuth, isAdmin, enableUser )
+router.get("/user/enable/:userId", isAuth, isAdmin, enableUser);
 
-module.exports = router
-
-
+module.exports = router;
